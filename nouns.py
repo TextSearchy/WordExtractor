24f13471ab0d05aa4e5e3a408c5e62f573b4a18c
Rahul Kolhatkar <rahul.kolhatkar96@gmail.com>
2016-09-26 07:41:59 +0530

    libraries added

--- nouns.py
+++ nouns.py
@@ -5,3 +5,8 @@ randomText = TextBlob("Same random text here will give the type/attribute of eve
 print(randomText.tags)		##Gives attribute of every word: Noun, Verb...
 blob = TextBlob(textinput)
 print(blob.noun_phrases)	##Gives only the noun phrases
+
+
+##pip install -U textblob
+##python -m textblob.download_corpora
+##pip install -U nltk => Not needed now, will be needed later, bery bery usephul.
